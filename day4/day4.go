cca88699ea7f41ff47ed884c2b5cc4697d685706
Kyle Harrison <kylejharrison@gmail.com>
2025-12-04 11:47:28 +0000

    day 4 part 2

--- day4/day4.go
+++ day4/day4.go
@@ -40,38 +40,48 @@ func main() {
 	colLen := len(rolls[0])
 
 	var ansRolls [][]string
-	for row := 0; row < len(rolls); row++ {
-		var ansRow []string
-		for col := 0; col < len(rolls[row]); col++ {
-			adjTotal := 0
-			// fmt.Println(row, col, rolls[row][col])
-			if rolls[row][col] != "@" {
-				ansRow = append(ansRow, ".")
-				// fmt.Println(".")
-				continue
-			}
-			for adjRow := row - 1; adjRow <= row + 1; adjRow++ {
-				for adjCol := col - 1; adjCol <= col + 1; adjCol++ {
-					if adjCol < 0 || adjCol == colLen || adjRow < 0 || adjRow == len(rolls) || (adjCol == col && adjRow == row) {
-						continue
-					}
-					if rolls[adjRow][adjCol] == "@" {
-						// fmt.Println(adjRow, adjCol)
-						adjTotal++
+	totalChanged := true
+	for totalChanged {
+		ansRolls = [][]string{}
+		startTotal := total
+		fmt.Println("checking, current total:", total)
+		for row := 0; row < len(rolls); row++ {
+			var ansRow []string
+			for col := 0; col < len(rolls[row]); col++ {
+				adjTotal := 0
+				// fmt.Println(row, col, rolls[row][col])
+				if rolls[row][col] != "@" {
+					ansRow = append(ansRow, ".")
+					// fmt.Println(".")
+					continue
+				}
+				for adjRow := row - 1; adjRow <= row+1; adjRow++ {
+					for adjCol := col - 1; adjCol <= col+1; adjCol++ {
+						if adjCol < 0 || adjCol == colLen || adjRow < 0 || adjRow == len(rolls) || (adjCol == col && adjRow == row) {
+							continue
+						}
+						if rolls[adjRow][adjCol] == "@" {
+							// fmt.Println(adjRow, adjCol)
+							adjTotal++
+						}
 					}
 				}
+				if adjTotal < 4 {
+					total++
+					// fmt.Println("x", adjTotal)
+					ansRow = append(ansRow, "x")
+				} else {
+					// fmt.Println("@", adjTotal)
+					ansRow = append(ansRow, "@")
+				}
 			}
-			if adjTotal < 4 {
-				total++
-				// fmt.Println("x", adjTotal)
-				ansRow = append(ansRow, "x")
-			} else {
-				// fmt.Println("@", adjTotal)
-				ansRow = append(ansRow, "@")
-			}
+			// fmt.Println(ansRow)
+			ansRolls = append(ansRolls, ansRow)
 		}
-		// fmt.Println(ansRow)
-		ansRolls = append(ansRolls, ansRow)
+		rolls = ansRolls
+		if total == startTotal {
+			totalChanged = false
+		}
 	}
 
 	fmt.Println(rolls)
