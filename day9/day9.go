2dd0cab6c36633f5351c2ca2737e662916699aad
Kyle Harrison <kylejharrison@gmail.com>
2025-12-10 15:02:28 +0000

    day9 part1

--- day9/day9.go
+++ day9/day9.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
 	"sort"
 	"strconv"
@@ -38,8 +37,8 @@ func area(p1 Point, p2 Point) int {
 		biggestY, smallestY = smallestY, biggestY
 	}
 
-	return (biggestX - smallestX) * (biggestY - smallestY)
-}
+	return (biggestX - smallestX + 1) * (biggestY - smallestY + 1)
+} 
 
 func main() {
 	fmt.Println("day 9 let's go!")
@@ -82,81 +81,5 @@ func main() {
 	}
 	sort.Sort(ByArea(pairings))
 
-	var clusters []map[int]struct{}
-
-	var i int
-
-	var lastPair Pairing
-	for len(clusters) == 0 || len(clusters[0]) != len(data) {
-		fmt.Println(pairings[i])
-		lastPair = pairings[i]
-		addedToExisting := false
-		if len(clusters) == 0 {
-			nodes := make(map[int]struct{})
-			nodes[pairings[i].P1] = struct{}{}
-			nodes[pairings[i].P2] = struct{}{}
-			clusters = append(clusters, nodes)
-		} else {
-			for c, cluster := range clusters {
-				var toAdd []int
-				if _, ok := cluster[pairings[i].P1]; ok {
-					toAdd = append(toAdd, pairings[i].P2)
-				}
-				if _, ok := cluster[pairings[i].P2]; ok {
-					toAdd = append(toAdd, pairings[i].P1)
-				}
-
-				for _, node := range toAdd {
-					cluster[node] = struct{}{}
-				}
-
-				if len(toAdd) == 1 {
-					addedToExisting = true
-					for c1 := c + 1; c1 < len(clusters); c1++ {
-						if _, ok := clusters[c1][toAdd[0]]; ok {
-							for node := range clusters[c1] {
-								cluster[node] = struct{}{}
-							}
-							clusters[c1] = make(map[int]struct{})
-							break
-						}
-					}
-				}
-				if len(toAdd) == 2 {
-					addedToExisting = true
-					break
-				}
-			}
-			if !addedToExisting {
-				nodes := make(map[int]struct{})
-				nodes[pairings[i].P1] = struct{}{}
-				nodes[pairings[i].P2] = struct{}{}
-				clusters = append(clusters, nodes)
-			}
-		}
-		i++
-	}
-
-	fmt.Println(clusters)
-
-	var lengths []int
-
-	for _, c := range clusters {
-		lengths = append(lengths, len(c))
-	}
-
-	sort.Sort(sort.Reverse(sort.IntSlice(lengths)))
-
-	fmt.Println(lengths)
-
-	var total int = 1
-
-	for i := range 3 {
-		total *= lengths[i]
-	}
-
-	fmt.Println(total)
-
-	fmt.Println(lastPair)
-	fmt.Println(data[lastPair.P1].X * data[lastPair.P2].X)
+	fmt.Println(pairings)
 }
